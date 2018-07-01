08f72892d3ea1b2877aae24207f66cc2c3aaeab4
Steven Mercatante <steven.mercatante@gmail.com>
2018-07-01 12:41:50 -0400

    Add RadioGrooup for controlling text color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
 import { ApolloProvider } from "react-apollo";
+import React from "react";
+import { RadioGroup, Radio } from "react-radio-group";
 import client from "./client";
 
-const Header = () => <header>I am the header</header>;
+const Header = () => (
+  <header>
+    <h2>I am the header</h2>
+  </header>
+);
 
 const ControlPanel = ({ children }) => <div>{children}</div>;
 
-const TextControls = () => <div>text controls</div>;
+const TextControls = () => (
+  <div>
+    <p>Set text color:</p>
+    <RadioGroup
+      name="textColor"
+      onChange={color => console.log(`Set the color to ${color}`)}
+    >
+      <Radio value="red" />Red
+      <Radio value="blue" />Blue
+      <Radio value="green" />Green
+    </RadioGroup>
+  </div>
+);
 
 const App = () => (
   <ApolloProvider client={client}>
